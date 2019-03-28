5ce3a5d897a04250fdfe8a5a0cb99524b6f8ac94
Mariu19 <mariu.gauvin@gmail.com>
2019-03-28 01:06:25 +0100

    Update front

--- index.php
+++ index.php
@@ -1,60 +1,85 @@
 <?php
 session_start();
-require 'jams/vue/entete.php';
+require 'vue/entete.php';
 ?>
 
 <!-- Texte sous la banniere  -->   
-<div class="container">
-    <h1 class="texteAccueil"><span class="jams">Défi écolo</span></h1>
-</div>
+<div class="container-fluid p-0" id="mesDefis">
+    <nav class="navbar navbar-light bg-light">
+        <h2 class="texteAccueil"><span class="jams">Mes défis</span></h1>
 
-<!-- Corps de la page divisé en 2 à gauche la recherche rapide, à droite la google map -->   
-<div class="container">
-    <div class="col-md-5">
-        <div class="row">
-            <h2>Créer un défi écolo</h2>
+        </nav>
+    </div>
 
-        </div>
+    <!-- Corps de la page divisé en 2 à gauche la recherche rapide, à droite la google map -->   
 
-        <div class="row">
-            <!-- Formulaire recherche rapide  -->   
-            <div class="form-group">
-                <label for="rechercherapide">Lieu du défi</label>
-                <input type="text" class="form-control" placeholder="Toulouse" name="villeDep" required="required" id="start">
+    <div class="container-fluid p-0" id="planDefi">
+        <nav class="navbar navbar-light bg-light">
+            <h2 class="texteAccueil"><span class="jams">Planifier un défi</span></h1>
+            </nav>
+
+            <div class="container">
+
+                            <div class="col-md-7">
+                <div class="row">
+                    <h2>Créer un défi écolo</h2>
+
+                </div>
+
+                <div class="row">
+                    <!-- Formulaire recherche rapide  -->   
+                    <div class="form-group">
+                        <label for="rechercherapide">Lieu du défi</label>
+                        <input type="text" class="form-control" placeholder="Toulouse" name="villeDep" required="required" id="start">
+                    </div>
+
+                    <button class="btn btn-default" onclick="creationDefi()">
+                        <span class="glyphicon glyphicon-search"></span> Créer Défi
+                    </button>
+
+
+                </div>
             </div>
-            <button class="btn btn-default" onclick="creationDefi()">
-                <span class="glyphicon glyphicon-search"></span> Créer Défi
-            </button>
 
+            <div>
+                <div class="row">
+                    <h3  id="map" style="width:100%;height:400px;"></h3>
+
+                </div>
+            </div>
+                
+
+            </div>
+
+
+        </div>
+
+        <div class="container-fluid" >
+            <nav class="navbar navbar-light bg-light">
+                <h1 class="texteAccueil"><span class="jams">Liste des défis</span></h1>
+            </nav>
+
+            <div class="col-md-4">
+            </div>
+
+            <div class="col-md-4" id="listeDefis">
+            </div>
             
-        </div>
-
-        <div class="row">
-            <h3  id="map" style="width:100%;height:400px;"></h3>
+            <div class="col-md-4">
+            </div>
 
         </div>
 
     </div>
-    <!-- Google map  -->   
-    <div class="col-md-1">
 
-    </div>
-
-    <div class="col-md-5">
-        <div class="row" id="listeDefis">
-            <h2>Liste défis</h2>
-      </div>
-
-  </div>
-</div>
-</div>
+<!--</div>-->
 
 
-<script src="/jams/js/carte.js"></script>
+<script src="js/carte.js"></script>
 <!-- JS google map  --> 
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDeOfMRGfOTltlJNVcNpF6TVXIUWtZzq5A&libraries=places&callback=initMap">
 </script>
 
 <?php
-require 'jams/vue/footer.php';
+require 'vue/footer.php';
 ?>
